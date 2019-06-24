296e37cbe35a12cf40ad3b2af8df048f21f85d5a
zhangjie <wlh520z@163.com>
2019-06-24 23:30:08 +0800

    调整登陆注册页面

--- django_food/polls/urls.py
+++ django_food/polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,re_path
 
-
+from polls.views import login, register
 
 urlpatterns=[
     # # re_path('get_register/$',get_register,name='get_register'),
     # re_path(r'^get_code/$', get_code, name='getcode'),
-    # re_path(r'^login/$',login,name='login'),
+    re_path(r'^login/$',login,name='login'),
+    re_path(r'^register/$', register, name='register'),
+
     # re_path(r'^register/$', Register.as_view(), name='register')
 ]
