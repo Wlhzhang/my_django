8f7e1438f118b9e2283e65b029706bf591e5a0b2
zhangjie <zhangjie@p1902.com>
2019-06-24 18:06:00 +0800

    项目第一天

---
+++ django_food/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,re_path
+
+
+
+urlpatterns=[
+    # # re_path('get_register/$',get_register,name='get_register'),
+    # re_path(r'^get_code/$', get_code, name='getcode'),
+    # re_path(r'^login/$',login,name='login'),
+    # re_path(r'^register/$', Register.as_view(), name='register')
+]
